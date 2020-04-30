b30588d7756564a1c37441413c47f174aa3de73a
First Last <email.mail.com>
2020-04-30 03:22:02 -0500

    Initial commit

---
+++ src/main/java/com/spring/microservices/registration/MicroservicesRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/microservicesregistration/MicroservicesRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservicesregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
